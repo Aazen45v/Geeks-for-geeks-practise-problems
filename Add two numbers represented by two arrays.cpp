67808be4ccf4f86a68d7da4187d1cc37f8c702d1
Mohammad Aazen <mohdaazen@gmail.com>
2023-09-14 13:05:59 +0530

    Update Add two numbers represented by two arrays.cpp
    Given two array A[0….N-1] and B[0….M-1] of size N and M respectively, representing two numbers such that every element of arrays represent a digit. For example, A[] = { 1, 2, 3} and B[] = { 2, 1, 4 } represent 123 and 214 respectively. The task is to find the sum of both the numbers.
    
    Example 1:
    
    Input : A[] = {1, 2}, B[] = {2, 1}
    Output : 33
    Explanation:
    N=2, and A[]={1,2}
    M=2, and B[]={2,1}
    Number represented by first array is 12.
    Number represented by second array is 21
    Sum=12+21=33
     
    
    Example 2:
    
    Input : A[] = {9, 5, 4, 9}, B[] = {2, 1, 4} 
    Output : 9763 
    Your Task:
    This is a function problem. The input is already taken care of by the driver code. You only need to complete the function calc_Sum() that takes an array (a), sizeOfArray (n), an array (b), sizeOfArray (m), and return the sum . The driver code takes care of the printing.
    
    Expected Time Complexity: O(N + M).
    Expected Auxiliary Space: O(N + M).

--- Add two numbers represented by two arrays.cpp
+++ Add two numbers represented by two arrays.cpp
@@ -1,3 +1,10 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
 class Solution{
     public:
     string calc_Sum(int *a,int n,int *b,int m){
@@ -27,6 +34,34 @@ class Solution{
         ans.push_back(digit+'0');
         j--;
     }
+    if(carry){
+        ans.push_back(carry +'0');
+    }
+    reverse(ans.begin(),ans.end());
         return ans;
     }
 };
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    int m;
+	    cin>>m;
+	    int b[m];
+	    for(int i=0;i<m;i++){
+	        cin>>b[i];
+	    }
+	    Solution ob;
+	    cout << ob.calc_Sum(a,n,b,m) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
